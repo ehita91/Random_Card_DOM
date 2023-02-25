2af651d5d2a50dd188ee9028bef1a33f087e69ec
Ernesto Hita <ehita91@gmail.com>
2023-02-25 01:26:46 +0000

    Finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  change();
+  const btn = document.querySelector("#btn");
+  btn.addEventListener("click", change);
 };
+
+function change() {
+  const center = document.querySelector("#number");
+  const tops = document.querySelector("#topsuite");
+  const bottoms = document.querySelector("#bottomsuite");
+  const suites = ["♦", "♥", "♠", "♣"];
+  const cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+  const suiteindex = Math.floor(Math.random() * suites.length);
+
+  center.innerText = cards[Math.floor(Math.random() * cards.length)];
+  tops.innerText = suites[suiteindex];
+  bottoms.innerText = suites[suiteindex];
+
+  if (suiteindex === 0 || suiteindex === 1) {
+    tops.style.color = "red";
+    bottoms.style.color = "red";
+  } else {
+    tops.style.color = "black";
+    bottoms.style.color = "black";
+  }
+}
